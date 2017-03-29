b7b01246c3a6905b4ce3eadbcb32f63c81239e86
Aaron Chan <achan1058@hotmail.com>
2017-03-28 19:32:32 -0700

    Added more stuff for Knights of Doom.

--- Gamebook Solutions.cpp
+++ Gamebook Solutions.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
+	string s;
+	//cin >> s;
+	int total = 0;
+	for (char c : s) {
+		if (isalpha(c))
+			total += tolower(c) - 'a' + 1;
+	}
+	cout << total << endl;
 }
